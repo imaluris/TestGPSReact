11805af91bea929e5019651edfbf1ff8b42ff8c5
imaluris <andrea.castellaniperelli@gmail.com>
2026-04-08 13:36:16 +0200

    add netifly redirects

--- src/data/points.js
+++ src/data/points.js
@@ -1,9 +1,9 @@
 export const points = [
   {
     id: 1,
-    name: "Punto 1 - Fontana",
-    lat: 45.464211,
-    lng: 9.191383,
+    name: "Punto 1 - Primo punto via mentana",
+    lat: 42.554030,
+    lng: 12.642713,
     text: "Sei arrivato al primo punto di interesse. Da qui parte la storia.",
     radius: 30, // metri
     path: "/",
@@ -11,9 +11,9 @@ export const points = [
   },
   {
     id: 2,
-    name: "Punto 2 - Piazza",
-    lat: 45.464800,
-    lng: 9.190900,
+    name: "Punto 2 - Paggi e serangeli",
+    lat: 42.555418,
+    lng: 12.643402,
     text: "Secondo punto raggiunto. La storia continua.",
     radius: 30,
     path: "/punto-2",
@@ -21,9 +21,9 @@ export const points = [
   },
   {
     id: 3,
-    name: "Punto 3 - Torre",
-    lat: 45.465300,
-    lng: 9.190300,
+    name: "Punto 3 - Passerella",
+    lat: 42.556040,
+    lng: 12.643572,
     text: "Ultimo punto raggiunto. Hai completato il test.",
     radius: 30,
     path: "/punto-3",
